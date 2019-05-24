2f3ba18f5e0552a8a25c111c61ec00fa2e9eeb95
Jui Chang <juichang@Juis-MacBook-Air.local>
2019-05-24 20:07:26 +0800

    pd df, before try grids in frame

--- combine.py
+++ combine.py
@@ -4,6 +4,14 @@
 # Label (output)
 
 from tkinter import *
+import MySQLdb
+import pandas as pd
+
+# 連接 MySQL 資料庫
+db = MySQLdb.connect(host="localhost",
+    user="root", passwd="rootroot", db="soccer")
+cursor = db.cursor(MySQLdb.cursors.DictCursor)
+
 
 top = Tk()
 
@@ -23,8 +31,8 @@ dlab2.config(font=("Courier", 20), bg='DarkSeaGreen1')
 dlab2.grid(row=1, column=0, sticky='nsew')
 
 om_str = StringVar()
-om_str.set("one")
-om = OptionMenu(top, om_str, "one", "two", "three", "four")
+om_str.set("MySQL")
+om = OptionMenu(top, om_str, "one", "two", "three", "four", "MySQL")
 om.config(width=60)
 # om.pack(side = RIGHT)
 om.grid(row=1, column=1, sticky='nsew')
@@ -37,7 +45,6 @@ dlab3.config(font=("Courier", 20), bg='hot pink')
 # dlab3.pack(side = LEFT)
 dlab3.grid(row=2, column=0, sticky='nsew')
 
-
 text = Text(top)
 text.config(width=85, highlightbackground='green')
 # text.pack(side=RIGHT)
@@ -47,19 +54,33 @@ text.grid(row=2, column=1, sticky='nsew')
 lab_str = StringVar()
 
 def but_func(event):
-    # print('but_func')
     om_value = om_str.get()
-    # entry_value = e.get()
     text_value = text.get("1.0",END)
 
     # if om_value == 'MySQL', send query(determined by om_value and entry_value) to mysql
+    if om_value == 'MySQL':
+        cursor.execute(text_value)
+    
+        results = cursor.fetchall()
+        # print("type of results:", type(results))
+        
+        df = pd.DataFrame(columns=results[0].keys())
+        print(len(results))
+        print(results[0].values())
+        for i in range(len(results)):
+            df.loc[i] = list(results[i].values())
 
+        print(df.to_string(index=False))
+        lab_str.set(df.to_string(index=False))
+        # lab_str.set(str(results[0].keys()) + '\n' + str(results[0].values()))
+            
     # else send the query commsnds depend on om_value (switch cases)
 
     # get the query result(in the way determined by om_value and entry_value) from mysql
 
     # output to GUI
-    lab_str.set(om_value + '\n' + text_value)
+    # lab_str.set(om_value + '\n' + text_value)
+    
 
 # row 4
 b = Button(top, text='Search', bg='yellow')
@@ -79,7 +100,8 @@ dlab4.grid(row=4, columnspan=2, ipady=20, sticky='nsew')
 
 # row 6
 lab_str.set('no content')
-lab = Label(top, textvariable=lab_str)
+lab = Label(top, textvariable=lab_str, anchor='e', justify=LEFT)
 lab.grid(row=5, columnspan=2, sticky='nsew')
+# lab.grid(row=5, sticky='nsew')
 
 top.mainloop()
